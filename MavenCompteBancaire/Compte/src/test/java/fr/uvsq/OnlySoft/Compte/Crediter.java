4f64ef9f24f22713c5c81b8ecee20f9907bf7d2e
RachData <rachidderra@gmail.com>
2019-11-10 17:56:22 +0000

    Configuration du POM

---
+++ MavenCompteBancaire/Compte/src/test/java/fr/uvsq/OnlySoft/Compte/Crediter.java
@@ -0,0 +1,31 @@
+package fr.uvsq.OnlySoft.Compte;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Crediter {
+	
+
+	@Test
+	public void testCrediter()
+	{
+		double solde=30.0;
+		double montant=10.2;
+				 
+		if (montant<=0) 
+		{
+			
+			assertEquals("montant inf � 0",10.0, 02, 0);
+		}
+		else
+		{	
+		solde=solde +montant;
+
+		assertEquals("montant sup � 0", 40.2, solde, 0);	
+		}
+
+	}
+	
+	
+}
---
+++ MavenCompteBancaire/Compte/src/test/java/fr/uvsq/OnlySoft/Compte/Debiter.java
@@ -0,0 +1,29 @@
+package fr.uvsq.OnlySoft.Compte;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Debiter {
+	
+		@Test
+		public void debiter()
+		{
+			double solde=30.0;
+			double montant=31;
+					 
+			if (montant<=0||montant>solde) 
+			{
+				
+				assertEquals("montant inf � 0",-1,solde-montant,0);
+			}
+			else
+			{	
+				solde=solde-montant;
+
+				assertEquals("montant sup � 0",19.8, solde, 0);	
+			}
+
+		}
+	
+}
